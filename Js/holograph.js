4f2574b7bb3e9f8d2e1da7c48648f3653007897a
WaleedRazaShoaib <146237300+WaleedRazaShoaib@users.noreply.github.com>
2024-04-30 03:44:59 +0500

    still working

---
+++ Js/holograph.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("js/holograph.json")
+      .then(response => response.json())
+      .then(result => {
+        let html = '';
+        result.products.forEach(abcproduct => {
+            html += `
+                <div class="col-md-3 mb-4 col-12 justify-content-between">
+                    <div class="card">
+                        <img class="card-img-top p-3 " src="${abcproduct.img}" alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">${abcproduct.description}</p>
+                        </div>
+                    </div>
+                </div>`;
+        });
+        document.querySelector(".desrows").innerHTML = html;
+      })
+      .catch(error => console.error("Error fetching images.json:", error));
+});
