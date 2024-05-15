ca54362fe4bc79a9af904262c35bc8ec49d0f42b
WaleedRazaShoaib <146237300+WaleedRazaShoaib@users.noreply.github.com>
2024-05-15 16:53:56 +0500

    Update holograph.js

--- Js/holograph.js
+++ Js/holograph.js
@@ -5,17 +5,16 @@
         let html = '';
         result.products.forEach(abcproduct => {
             html += `
-                <div class="col-md-3 mb-4 col-12 justify-content-between">
-                    <div class="card">
-                        <img class="card-img-top p-3 " src="${abcproduct.img}" alt="Card image cap">
-                        <hr>
-                        <div class="card-body">
-                        <h5 class="card-title fs-3 firsttext">${abcproduct.name}</h5>
-                            <p class="card-text">${abcproduct.description}</p>
-                            <a  class="addtocart">Add to Cart</a>
-                        </div>
-                    </div>
-                </div>`;
+            <div class="col-md-3 mb-4 col-12 justify-content-between">
+            <div class="card">
+                <img  src="${abcproduct.img}" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title firsttext fs-4">${abcproduct.name}</h5>
+                    <p class="card-text">${abcproduct.description}</p>
+                    <a  class="addtocart">Add to Cart</a>
+                </div>
+            </div>
+        </div>`;
         });
         document.getElementById("desrows").innerHTML = html;
       })
